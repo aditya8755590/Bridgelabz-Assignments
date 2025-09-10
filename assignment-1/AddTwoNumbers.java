a0405d6731df63711fe0602f0e911f346627406d
aditya8755590 <aditya.kumar875590@gmail.com>
2025-09-10 11:45:41 +0530

    ad

--- assignment-1/AddTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-class AddTwoNumbers {
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
-    }
-}
